e0eb8ea2db11b3ef339272f445ff28a649e8760d
Boomrock <63188345+Boomrock@users.noreply.github.com>
2021-12-02 18:27:33 +0700

    main

---
+++ idz13/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+namespace idz13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i <= 10; i++)
+            {
+                label1.Text += i + " " + method(i) + "\n";
+            }
+        }
+        public int method(int x)
+        {
+            if(x %2 == 0)
+            {
+                x = -x;
+            }
+            return x;
+        }
+    }
+}
